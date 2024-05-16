e9d051c5c3cef67e1792b82327c997c0411bff75
Adamz <91496585+GiovanniAz@users.noreply.github.com>
2024-05-15 22:38:44 -0500

    script.js

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-// Selecciona la opción del botón desplegable y reemplaza el texto del botón principal
 function selectOption(optionText) {
     var dropdownBtn = document.querySelector('.dropbtn');
     dropdownBtn.innerText = optionText;
-    toggleDropdown(); // Cierra el menú desplegable después de seleccionar una opción
+    toggleDropdown(); 
   }
   
-  // Muestra u oculta el menú desplegable
   function toggleDropdown() {
     var dropdown = document.getElementById("myDropdown");
     dropdown.classList.toggle("show");
   }
   
-  // Cierra el menú desplegable si se hace clic fuera de él
   window.onclick = function(event) {
     if (!event.target.matches('.dropbtn') && !event.target.matches('.dropdown-content a')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
