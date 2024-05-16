51eecc7f463eedeecbaa5b2cba8494a0ee74d932
Adamz <91496585+GiovanniAz@users.noreply.github.com>
2024-05-15 22:38:10 -0500

    conexion.php

--- conexion.php
+++ conexion.php
@@ -1,22 +1,19 @@
 <?php
-$servername = "localhost"; // Cambia esto por la dirección de tu servidor MySQL
-$username = "root"; // Cambia esto por tu nombre de usuario de MySQL
-$password = ""; // Cambia esto por tu contraseña de MySQL
-$dbname = "Seguros"; // Cambia esto por el nombre de tu base de datos
+$servername = "localhost"; 
+$username = "root"; 
+$password = ""; 
+$dbname = "Seguros";
 
-// Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Comprobar la conexión
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 
-// Consulta SQL para obtener las marcas
 $sql = "SELECT marca FROM marca";
 $result = $conn->query($sql);
 
-// Generar opciones del menú desplegable
+
 $options = "";
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
@@ -26,6 +23,5 @@ if ($result->num_rows > 0) {
     echo "No se encontraron marcas en la base de datos.";
 }
 
-// Cerrar conexión
 $conn->close();
 ?>
